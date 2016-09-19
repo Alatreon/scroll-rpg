d9ef504b8d2c8a9c76ae2e5ccee6213e3a5e82e7
Alateon <dc.shoes@hotmail.fr>
2016-09-20 00:27:58 +0200

    finition des animations

--- js/perso.js
+++ js/perso.js
@@ -19,53 +19,54 @@ function Perso ()
 	/*animation*/
 	this.jumpanim=0;
 	this.leftright=0;
+	this.leftrightAnim=0;
+	this.incrMoveAnim=0;
 
-	this.color = 'blue';
-
+	/*Saut*/
 	this.JumpSize = 86;
 	this.jumpPos = 0;
+	this.hautBas = true;
+
 }
 Perso.prototype = 
 {
 	draw : function ()
 	{	
-		Self.ctx.drawImage(Self.LoadImage.loadedImgList[1],this.jumpanim+this.leftright,0,this.width,this.height,this.x,this.y,this.width,this.height);
+		Self.ctx.drawImage(Self.LoadImage.loadedImgList[1],this.jumpanim+this.leftright+this.leftrightAnim,0,this.width,this.height,this.x,this.y,this.width,this.height);
 	},
 	jump : function (callback) 
 	{
 		self=this		
 		/*Mise en place des variables qui vont determiner la position du saut*/
-		var hautBas = true;
 
 		/*animation du saut*/
 		this.heroMoveJumpInter = setInterval(function()
 		{
 			self.jumpPos+=1;
 
-			if( hautBas == true && self.jumpPos < self.JumpSize )
+			if( self.hautBas == true && self.jumpPos < self.JumpSize )
 			{
 				self.jumpanim=320;
 
 				self.y -= self.vy;
 			}
-			if( hautBas == true && self.jumpPos == self.JumpSize )
+			if( self.hautBas == true && self.jumpPos == self.JumpSize )
 			{
 				self.jumpanim=0;
 
-				hautBas = false;
+				self.hautBas = false;
 			}
-			if( hautBas == false && self.jumpPos < self.JumpSize*2 )
+			if( self.hautBas == false && self.jumpPos < self.JumpSize*2 )
 			{
 				self.jumpanim=0;
 				
 				self.y += self.vy;
 			}
-			if( hautBas == false && self.jumpPos == self.JumpSize*2 )
+			if( self.hautBas == false && self.jumpPos == self.JumpSize*2 )
 			{
 				self.jumpanim=0;
-				
-				self.jumpanim=false;
-				hautBas = true;
+
+				self.hautBas = true;
 				self.jumpPos = 0;
 				self.y=Self.Map.floorVal;
 				callback();
@@ -80,14 +81,41 @@ Perso.prototype =
 	{
 		this.x += this.vx;
 		this.leftright=0;
+		this.moveAnim();
 	},
 	right : function () 
 	{
 		this.x -= this.vx;
 		this.leftright=1344;
+		this.moveAnim();
 	},
 	stop : function (evt) 
 	{
+		this.leftrightAnim=0;
 		Self.drawAll();
+	},
+	moveAnim : function ()
+	{
+		this.incrMoveAnim+=1;
+
+		if(this.incrMoveAnim==1 || this.incrMoveAnim>this.vx*5)
+		{
+			this.moveAnimIncr();
+			if(this.incrMoveAnim>this.vx*5){this.incrMoveAnim=0}			
+		}
+
+	},
+	moveAnimIncr : function ()
+	{		
+		if(this.leftrightAnim<192 && Self.KeyboardKey.heroMoveJumpBool==true)
+		{
+			this.leftrightAnim+=64;
+			this.incrMoveAnim=10;
+		}
+		else
+		{
+			this.leftrightAnim=0;
+
+		}
 	}
 }
